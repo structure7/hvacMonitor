bd7915f82c84c2c8ccb1565b7cf0796671ffcea6
structure7 <kemperm@usa.net>
2016-04-25 17:42:33 -0700

    Weather from Weather Underground API

--- hvacMonitor.ino
+++ hvacMonitor.ino
@@ -5,9 +5,11 @@
 #include <BlynkSimpleEsp8266.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <TimeLib.h>
+#include <TimeLib.h> //Can this go away?
 #include <WidgetRTC.h>
 #define ONE_WIRE_BUS 2
+#include <ArduinoJson.h>
+
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
@@ -15,9 +17,28 @@ DallasTemperature sensors(&oneWire);
 DeviceAddress ds18b20RA = { 0x28, 0xEF, 0x97, 0x1E, 0x00, 0x00, 0x80, 0x54 }; // Return air probe
 DeviceAddress ds18b20SA = { 0x28, 0xF1, 0xAC, 0x1E, 0x00, 0x00, 0x80, 0xE8 }; // Supply air probe
 
-char auth[] = "fromBlynkApp *04";
+char auth[] = "fromBlynkApp";
 
 SimpleTimer timer;
+
+// The original time entries that didn't like being removed.
+#define DELAY_NORMAL    (10)
+#define DELAY_ERROR     (10)
+
+//#define WUNDERGROUND "www.mk.com"  // Keep for test/debug
+#define WUNDERGROUND "api.wunderground.com"
+
+// HTTP request
+const char WUNDERGROUND_REQ[] =
+  //"GET /test.json HTTP/1.1\r\n"  // Keep for test/debug
+  "GET /api/[myAPIkey]/conditions/q/pws:KAZTEMPE29.json HTTP/1.1\r\n"
+  "User-Agent: ESP8266/0.1\r\n"
+  "Accept: */*\r\n"
+  "Host: " WUNDERGROUND "\r\n"
+  "Connection: close\r\n"
+  "\r\n";
+
+float temp_f;
 
 WidgetLED led1(V2);
 WidgetLCD lcd(V5);
@@ -33,8 +54,8 @@ int alarmFor = 0;
 unsigned long onNow = now();
 unsigned long offNow = now(); // To prevent error on first Tweet after blower starts - NOT WORKING YET
 
-int offHour, offHour24, onHour, onHour24, offMinute, onMinute, offSecond, onSecond, offMonth, onMonth, 
-offDay, onDay,runTime, tempSplit, secondsCount, alarmTime;
+int offHour, offHour24, onHour, onHour24, offMinute, onMinute, offSecond, onSecond, offMonth, onMonth,
+    offDay, onDay, runTime, tempSplit, secondsCount, alarmTime;
 
 void setup()
 {
@@ -45,14 +66,130 @@ void setup()
   sensors.setResolution(ds18b20RA, 10);
   sensors.setResolution(ds18b20SA, 10);
 
-  timer.setInterval(2500L, sendTemps); // Temperature sensor polling interval
-  timer.setInterval(2500L, sendLCDstatus); // Blower fan status polling interval
-  timer.setInterval(5000L, sendHeartbeat); // Blinks Blynk LED to reflect online status
-
   while (Blynk.connect() == false) {
     // Wait until connected
   }
   rtc.begin();
+
+  timer.setInterval(2500L, sendTemps); // Temperature sensor polling interval
+  timer.setInterval(2500L, sendLCDstatus); // Blower fan status polling interval
+  timer.setInterval(5000L, sendHeartbeat); // Blinks Blynk LED to reflect online status
+  timer.setInterval(300000L, sendWU); // 5 minutes between Wunderground API calls.
+  timer.setInterval(90000L, sendWUtoBlynk); // 1.5 minutes between API data updates to Blynk.
+
+   static char respBuf[4096];
+}
+
+static char respBuf[4096];
+
+bool showWeather(char *json);
+
+void sendWUtoBlynk()
+{
+    Blynk.virtualWrite(12, temp_f);
+}
+
+void sendWU()
+{
+  // Open socket to WU server port 80
+  Serial.print(F("Connecting to "));
+  Serial.println(WUNDERGROUND);
+
+  // Use WiFiClient class to create TCP connections (A LITTLE WORRIED HOW THIS WILL WORK WITH BLYNK)
+  WiFiClient httpclient;
+  const int httpPort = 80;
+  if (!httpclient.connect(WUNDERGROUND, httpPort)) {
+    Serial.println(F("connection failed"));
+    delay(DELAY_ERROR);
+    return;
+  }
+
+  // This will send the http request to the server
+  Serial.print(WUNDERGROUND_REQ);
+  httpclient.print(WUNDERGROUND_REQ);
+  httpclient.flush();
+
+  // Collect http response headers and content from Weather Underground
+  // HTTP headers are discarded.
+  // The content is formatted in JSON and is left in respBuf.
+  int respLen = 0;
+  bool skip_headers = true;
+  while (httpclient.connected() || httpclient.available()) {
+    if (skip_headers) {
+      String aLine = httpclient.readStringUntil('\n');
+      //Serial.println(aLine);
+      // Blank line denotes end of headers
+      if (aLine.length() <= 1) {
+        skip_headers = false;
+      }
+    }
+    else {
+      int bytesIn;
+      bytesIn = httpclient.read((uint8_t *)&respBuf[respLen], sizeof(respBuf) - respLen);
+      Serial.print(F("bytesIn ")); Serial.println(bytesIn);
+      if (bytesIn > 0) {
+        respLen += bytesIn;
+        if (respLen > sizeof(respBuf)) respLen = sizeof(respBuf);
+      }
+      else if (bytesIn < 0) {
+        Serial.print(F("read error "));
+        Serial.println(bytesIn);
+      }
+    }
+    delay(1);
+  }
+  httpclient.stop();
+
+  if (respLen >= sizeof(respBuf)) {
+    Serial.print(F("respBuf overflow "));
+    Serial.println(respLen);
+    delay(DELAY_ERROR);
+    return;
+  }
+  // Terminate the C string
+  respBuf[respLen++] = '\0';
+  Serial.print(F("respLen "));
+  Serial.println(respLen);
+  //Serial.println(respBuf);
+
+
+// This part will be removed in favor of the timer feature. Probably remove delay_error and just report ERR.
+  if (showWeather(respBuf)) {
+    delay(DELAY_NORMAL);
+  }
+  else {
+    delay(DELAY_ERROR);
+  }
+
+}
+
+bool showWeather(char *json)
+{
+  StaticJsonBuffer<3*1024> jsonBuffer;
+
+  // Skip characters until first '{' found
+  // Ignore chunked length, if present
+  char *jsonstart = strchr(json, '{');
+  //Serial.print(F("jsonstart ")); Serial.println(jsonstart);
+  if (jsonstart == NULL) {
+    Serial.println(F("JSON data missing"));
+    return false;
+  }
+  json = jsonstart;
+
+  // Parse JSON
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if (!root.success()) {
+    Serial.println(F("jsonBuffer.parseObject() failed"));
+    return false;
+  }
+
+  // Extract weather info from parsed JSON
+  JsonObject& current = root["current_observation"];
+  temp_f = current["temp_f"];  // Was `const float temp_f = current["temp_f"];`
+  Serial.print(temp_f, 1); Serial.print(F(" F "));
+
+  return true;
 }
 
 void sendTemps()
@@ -143,7 +280,7 @@ void sendLCDstatus()
   {
     //lcd.clear(); // Runs when blower is ON.
     lcd.print(0, 0, "  HVAC ON since  ");
-    
+
     if (onHour < 10)
     {
       lcd.print(1, 1, onHour);
